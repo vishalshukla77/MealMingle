895190c2cf7b21d2e7dff2c09945709546b387b9
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2024-07-09 19:57:12 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -17,7 +17,7 @@ const Cart = () => {
   const handleCheckOut = async () => {
     let userEmail = localStorage.getItem("userEmail");
 
-    let response = await fetch("http://localhost:5000/api/orderData", {
+    let response = await fetch("https://food-delivery-web-pvcg.onrender.com/api/orderData", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
