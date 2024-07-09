f6058a40b16b7af7fe7cdab9d677c89c2c683a7e
vishalshukla77 <121348446+vishalshukla77@users.noreply.github.com>
2024-07-09 19:58:37 +0530

    Update MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -8,7 +8,7 @@ export default function MyOrder() {
   const fetchMyOrder = async () => {
     const userEmail = localStorage.getItem('userEmail');
     try {
-      const response = await fetch("http://localhost:5000/api/myorderData", {
+      const response = await fetch("https://food-delivery-web-pvcg.onrender.com/api/myorderData", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
